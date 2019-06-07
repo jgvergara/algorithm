c0060643dec97ce1bf123196b7f2ffc840a49c5c
JmGV <47333104+jgvergara@users.noreply.github.com>
2019-06-07 15:05:40 +0200

    Testing with Clion

---
+++ Base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jmgv on 7/06/19.
+//
+
+#include "Base.h"
---
+++ Base.h
@@ -0,0 +1,14 @@
+//
+// Created by jmgv on 7/06/19.
+//
+
+#ifndef ALGORITHM_BASE_H
+#define ALGORITHM_BASE_H
+
+
+class Base {
+
+};
+
+
+#endif //ALGORITHM_BASE_H
